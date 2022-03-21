2cab82710f273f2bbc4a82db54fdd7a61b7e4edd
Mohammed-16 <mohammed.lokhandwala@vijiti.com>
2022-03-21 13:38:51 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { RedirectUriComponent } from './redirect-uri/redirect-uri.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     RedirectUriComponent
   ],
   imports: [
